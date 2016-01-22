c267b59c876904bcc1cd3519e7030d195b7b60b9
Rafa <rafag.1858@hotmail.es>
2016-01-22 07:19:47 +0100

    Añadida la clase RenderManager.

---
+++ include/ESE/Core/RenderManager.hpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   RenderManager.hpp
+ * Author: Rafa
+ *
+ * Created on 29 de agosto de 2015, 19:59
+ */
+
+#ifndef RENDERMANAGER_HPP
+#define	RENDERMANAGER_HPP
+
+#include <queue>
+
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Drawable.hpp>
+#include <ESE/Core/Layer.hpp>
+
+namespace ESE {
+    /**
+     * @brief Esta clase permite organizar el dibujado de objetos en función de
+     * su profundidad.
+     */
+    class RenderManager : public sf::Drawable{
+    public:
+        RenderManager();
+        virtual ~RenderManager();
+
+        void addDrawable(sf::Drawable & drawable, int z = 0);
+        void removeDrawable(sf::Drawable & drawable);
+        void moveDrawable(sf::Drawable & drawable, int newZ);
+
+        void draw(sf::RenderTarget & target, sf::RenderStates states) const;
+    private:
+        std::vector<ESE::Layer> layers;
+    };
+
+}
+
+#endif	/* RENDERMANAGER_HPP */
+
