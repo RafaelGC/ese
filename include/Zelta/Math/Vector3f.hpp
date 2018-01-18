ef7aaddefecce9af2ef9627b65fea2034bae6914
Rafa <rafa@rafadebian.home>
2018-01-18 13:42:58 +0100

    Añadido el módulo Math con utilidades de álgebra lineal.

---
+++ include/Zelta/Math/Vector3f.hpp
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Vector3f.hpp
+ * Author: rafa
+ *
+ * Created on January 5, 2018, 12:14 PM
+ */
+
+#ifndef ZELTALIB_VECTOR3F_HPP
+#define ZELTALIB_VECTOR3F_HPP
+
+namespace zt {
+    class Vector3f {
+        public:
+        Vector3f();
+        Vector3f(float x, float y, float z);
+
+        float getX() const;
+        float getY() const;
+        float getZ() const;
+
+        private:
+        float x, y, z;
+    };
+}
+
+#endif /* VECTOR3F_HPP */
+
